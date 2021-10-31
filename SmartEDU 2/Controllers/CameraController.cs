9f31c55277040a90b8a89e8fb61e121ffec64584
thelucpro1306 <ltl.luongtheluc@gmail.com>
2021-10-31 20:53:12 +0700

    Luc up len

---
+++ SmartEDU 2/Controllers/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartEDU_2.Controllers
+{
+    public class CameraController : Controller
+    {
+        // GET: Camera
+        public bool IsLoggined()
+        {
+            if (Session["TAIKHOAN"] != null)
+            {
+          
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public ActionResult Index()
+        {
+            Session["ReturnUrl"] = Url.Action("Index", "Camera");
+
+            if (IsLoggined() == true)
+            {
+                return View();
+            }
+            else
+                return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ SmartEDU 2/Controllers/HealthController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SmartEDU_2.Models;
+
+namespace SmartEDU_2.Controllers
+{
+    public class HealthController : Controller
+    {
+        LinQDataContext data = new LinQDataContext();
+
+        // GET: Health
+        public bool IsLoggined()
+        {
+            if (Session["TAIKHOAN"] != null)
+            {
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+     
+        public ActionResult Index()
+        {
+
+            Session["ReturnUrl"] = Url.Action("Index", "Health");
+
+            if (IsLoggined() == true)
+            {
+                TAIKHOAN tk = (TAIKHOAN)Session["TAIKHOAN"];
+                int idTk = tk.ID;
+      
+                var health = data.HOCSINHs.Select(a => a).Where(a => a.ID == idTk);
+                return View(health);
+            }
+            else
+                return RedirectToAction("Login", "Account");
+        }
+     
+    }
+}
